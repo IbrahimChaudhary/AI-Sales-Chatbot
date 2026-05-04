4f7513189712b872de5f3a5fe3be15e6abd227ef
Ahsan Javed <ahsanjavedd17@gmail.com>
2026-05-04 22:08:38 +0500

    Fix build issues

--- app/(app)/products/page.tsx
+++ app/(app)/products/page.tsx
@@ -31,6 +31,7 @@ import {
 } from "@/components/ui/select";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Product } from "@/lib/types/database";
+import { ProductsSkeleton } from "@/components/products/products-skeleton";
 
 export default function ProductsPage() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -278,50 +279,5 @@ export default function ProductsPage() {
         </DialogContent>
       </Dialog>
     </div>
-  );
-}
-
-{/* PRODUCT SKELETON */}
-interface ProductsSkeletonProps {
-  rows?: number;
-}
-
-export function ProductsSkeleton({ rows = 6 }: ProductsSkeletonProps) {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Category</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Created At</TableHead>
-          <TableHead className="text-right">Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Array.from({ length: rows }).map((_, i) => (
-          <TableRow key={i}>
-            <TableCell>
-              <Skeleton className="h-4 w-32" />
-            </TableCell>
-            <TableCell>
-              <Skeleton className="h-4 w-24" />
-            </TableCell>
-            <TableCell>
-              <Skeleton className="h-4 w-16" />
-            </TableCell>
-            <TableCell>
-              <Skeleton className="h-4 w-20" />
-            </TableCell>
-            <TableCell className="text-right">
-              <div className="flex justify-end gap-2">
-                <Skeleton className="h-8 w-8 rounded-md" />
-                <Skeleton className="h-8 w-8 rounded-md" />
-              </div>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
   );
 }
